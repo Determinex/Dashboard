b071929943a5ba4222ca5af4308a54b34f1affae
Determinex <s05966972@gmail.com>
2025-01-27 23:14:08 -0600

    Updated ajax_calls.js
    included a configuration dictionary for common variables and their values.

--- flask_server/static/js/ajax_calls.js
+++ flask_server/static/js/ajax_calls.js
@@ -1,69 +1,110 @@
+const config = {
+    apiBaseUrl: '/',
+    commands: {
+        setMasterPassword: 'setMasterPassword',
+        login: 'login',
+        addSecureData: 'addSecureData',
+        getSecureData: 'getSecureData',
+        encryptFile: 'encryptFile',
+        decryptFile: 'decryptFile'
+    },
+    selectors: {
+        masterPasswordInput: '#master-password',
+        loginPasswordInput: '#login-password',
+        dataTypeInput: '#data-type',
+        dataInput: '#data-input',
+        dataOutput: '#data-output',
+        fileInput: '#file-input'
+    }
+};
+
 $(document).ready(function() {
-    $('#set-password-button').click(function() {
-        const password = $('#master-password').val();
+    // Main function to handle AJAX calls
+    function ajaxCall(command, params) {
+        let url;
+        switch (command) {
+            case config.commands.setMasterPassword:
+                url = `${config.apiBaseUrl}set_master_password`;
+                break;
+            case config.commands.login:
+                url = `${config.apiBaseUrl}login`;
+                break;
+            case config.commands.addSecureData:
+                url = `${config.apiBaseUrl}add_secure_data`;
+                break;
+            case config.commands.getSecureData:
+                url = `${config.apiBaseUrl}get_secure_data`;
+                break;
+            case config.commands.encryptFile:
+                url = `${config.apiBaseUrl}encrypt_file`;
+                break;
+            case config.commands.decryptFile:
+                url = `${config.apiBaseUrl}decrypt_file`;
+                break;
+            default:
+                console.error('Invalid command');
+                return;
+        }
+
         $.ajax({
-            url: '/set_master_password',
+            url: url,
             method: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify({ password: password }),
+            data: JSON.stringify(params),
             success: function(response) {
-                alert(response.result);
+                if (response.result) {
+                    alert(response.result);
+                } else if (response.data) {
+                    $(config.selectors.dataOutput).text(JSON.stringify(response.data));
+                }
             },
             error: function(response) {
                 alert(response.responseJSON.error);
             }
         });
+    }
+
+    // Event listeners
+    $(config.selectors.masterPasswordInput).on('click', function() {
+        const password = $(config.selectors.masterPasswordInput).val();
+        ajaxCall(config.commands.setMasterPassword, { password: password });
     });
 
-    $('#login-button').click(function() {
-        const password = $('#login-password').val();
-        $.ajax({
-            url: '/login',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ password: password }),
-            success: function(response) {
-                alert(response.result);
-                $('#data-section').show();
-            },
-            error: function(response) {
-                alert(response.responseJSON.error);
-            }
-        });
+    $(config.selectors.loginPasswordInput).on('click', function() {
+        const password = $(config.selectors.loginPasswordInput).val();
+        ajaxCall(config.commands.login, { password: password });
     });
 
     $('#add-data-button').click(function() {
-        const dataType = $('#data-type').val();
-        const data = $('#data-input').val();
-        const password = $('#login-password').val();
-        $.ajax({
-            url: '/add_secure_data',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ data_type: dataType, data: data, password: password }),
-            success: function(response) {
-                alert(response.result);
-            },
-            error: function(response) {
-                alert(response.responseJSON.error);
-            }
-        });
+        const dataType = $(config.selectors.dataTypeInput).val();
+        const data = $(config.selectors.dataInput).val();
+        const password = $(config.selectors.loginPasswordInput).val();
+        ajaxCall(config.commands.addSecureData, { data_type: dataType, data: data, password: password });
     });
 
     $('#get-data-button').click(function() {
-        const dataType = $('#data-type').val();
-        const password = $('#login-password').val();
-        $.ajax({
-            url: '/get_secure_data',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ data_type: dataType, password: password }),
-            success: function(response) {
-                $('#data-output').text(JSON.stringify(response.data));
-            },
-            error: function(response) {
-                alert(response.responseJSON.error);
-            }
-        });
+        const dataType = $(config.selectors.dataTypeInput).val();
+        const password = $(config.selectors.loginPasswordInput).val();
+        ajaxCall(config.commands.getSecureData, { data_type: dataType, password: password });
+    });
+
+    // Example for file encryption
+    $('#encrypt-file-button').click(function() {
+        const fileData = $(config.selectors.fileInput).prop('files')[0]; // Assuming you have a file input
+        const password = $(config.selectors.loginPasswordInput).val();
+        const formData = new FormData();
+        formData.append('file', fileData);
+        formData.append('password', password);
+        ajaxCall(config.commands.encryptFile, formData);
+    });
+
+    // Example for file decryption
+    $('#decrypt-file-button').click(function() {
+        const fileData = $(config.selectors.fileInput).prop('files')[0]; // Assuming you have a file input
+        const password = $(config.selectors.loginPasswordInput).val();
+        const formData = new FormData();
+        formData.append('file', fileData);
+        formData.append('password', password);
+        ajaxCall(config.commands.decryptFile, formData);
     });
 });
