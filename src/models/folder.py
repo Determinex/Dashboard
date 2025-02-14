d2bc00850d0c1cc541e72aeecd13166513282f9e
Determinex <s05966972@gmail.com>
2025-02-14 13:51:40 -0600

    added folder structure traversal function definitions

--- src/models/folder.py
+++ src/models/folder.py
@@ -5,6 +5,24 @@ class Folder(db.Model):
     __tablename__ = 'folders'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
+    parent_id = db.Column(db.Integer, db.ForeignKey('folders.id'), nullable=True)  # Self-referential foreign key
+
+    parent = db.relationship('Folder', remote_side=[id], backref='subfolders')  # Relationship to allow parent-child access
 
     def __repr__(self):
         return f'<Folder {self.name}>'
+
+    def get_ancestors(self):
+        ancestors = []
+        current_folder = self.parent
+        while current_folder:
+            ancestors.append(current_folder)
+            current_folder = current_folder.parent
+        return ancestors
+
+    def get_descendants(self):
+        descendants = []
+        for subfolder in self.subfolders:
+            descendants.append(subfolder)
+            descendants.extend(subfolder.get_descendants())  # Recursively add subfolder descendants
+        return descendants
