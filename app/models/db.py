4f73e65cfced5b14d7389b1d2b7902c963d59178
Determinex <s05966972@gmail.com>
2025-02-28 23:11:33 -0600

    Update db.py

--- app/models/db.py
+++ app/models/db.py
@@ -1,4 +1,4 @@
-# src/models/db.py
+# app/models/db.py
 from flask_sqlalchemy import SQLAlchemy
 
 # Initialize the database instance
@@ -6,3 +6,7 @@ db = SQLAlchemy()
 
 class Model(db.Model):
     __abstract__ = True  # This class will not be instantiated directly
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db.session.remove()
