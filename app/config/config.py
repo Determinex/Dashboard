a3ac046a9e2b33c376a8935936cbaedce1894816
Determinex <s05966972@gmail.com>
2025-03-01 13:30:58 -0600

    Update config.py

--- app/config/config.py
+++ app/config/config.py
@@ -8,28 +8,57 @@ from dotenv import load_dotenv
 # Load environment variables from .env
 load_dotenv()
 
-class Config:
-    """Base configuration class."""
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
+class DirectoryConfig:
+    """Directory configuration class."""
+    
     BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
     DATABASE_DIR = os.path.join(BASE_DIR, 'database')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'sqlite:///{os.path.join(DATABASE_DIR, "dashboard.db")}')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
+    LOGS_DIR = os.path.join(BASE_DIR, 'logs')
+
+    @staticmethod
+    def ensure_directories_exist():
+        """Ensure that necessary directories exist."""
+        for directory in [DirectoryConfig.DATABASE_DIR, DirectoryConfig.UPLOAD_FOLDER, DirectoryConfig.LOGS_DIR]:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+                logging.info(f"Created directory at {directory}")
+
+class Config:
+    """Base configuration class."""
+    
+    # Sensitive Configuration
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # This will be overridden in DevConfig
+    API_KEY = os.getenv('API_KEY')
+
+    # General Configuration
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB
 
+    # Default values for non-sensitive variables
     DEBUG = os.getenv('DEBUG', 'False') == 'True'
     LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 
     # Ensure the necessary directories exist
-    for directory in [DATABASE_DIR, UPLOAD_FOLDER, os.path.join(BASE_DIR, 'logs')]:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-            logging.info(f"Created directory at {directory}")
+    DirectoryConfig.ensure_directories_exist()
 
     @staticmethod
     def load_json_settings(filename):
         """Load JSON settings from a file."""
         with open(filename, 'r') as f:
             return json.load(f)
+
+class DevConfig(Config):
+    """Development configuration class."""
+    DEBUG = True  # Enable debug mode
+    # Specify the local SQLite database URI directly
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DirectoryConfig.DATABASE_DIR, 'dashboard.db')
+    LOG_LEVEL = 'DEBUG'  # Set log level to DEBUG for development
+
+class ProdConfig(Config):
+    """Production configuration class."""
+    DEBUG = False  # Disable debug mode
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # Use the environment variable for production
+    LOG_LEVEL = 'ERROR'  # Set log level to ERROR for production
