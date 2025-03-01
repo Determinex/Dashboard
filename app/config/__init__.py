e5d56bd9ae2b69e65250e9db1a2fe167588f021e
Determinex <s05966972@gmail.com>
2025-03-01 13:39:22 -0600

    Update __init__.py

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,6 +1,6 @@
 # app/config/__init__.py
 
-from .config import Config
+from .config import DevConfig, ProdConfig
 
 # Load site settings
 site_settings = Config.load_json_settings('app/config/site-settings.json')
