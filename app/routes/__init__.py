5a10c2b5d72299a6d0afa707221cbeedb003cc87
Determinex <s05966972@gmail.com>
2025-02-28 23:24:11 -0600

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,17 @@
+# app/routes/__init__.py
+
 from flask import Blueprint
 
-bp = Blueprint('main', __name__)
+# Import all your routes here
+from .auth import auth_bp
+from .bookmarks_route import bookmarks_bp
+from .notes_route import notes_bp
+from .passwords_route import passwords_bp
+from .main import main_bp
 
-from . import main
+# Create a list of all blueprints
+blueprints = [auth_bp, bookmarks_bp, notes_bp, passwords_bp, main_bp]
+
+def register_routes(app):
+    for blueprint in blueprints:
+        app.register_blueprint(blueprint)
